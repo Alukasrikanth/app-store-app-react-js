d07a6b65b573c93c704fe268ac12a16b178b6612
Alukasrikanth <srikanthaluka97@gmail.com>
2022-07-05 18:25:25 +0000

    app-stote-ap

---
+++ src/components/TabItem/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const TabItem = props => {
+  const {tabDetails, updatedActiveId, isActive} = props
+  const {displayText, tabId} = tabDetails
+  const onClickTabItem = () => {
+    updatedActiveId(tabId)
+  }
+  const isActiveBtn = isActive ? 'tab-btn active' : 'tab-btn'
+  return (
+    <li className="tab-item">
+      <button type="button" className={isActiveBtn} onClick={onClickTabItem}>
+        {displayText}
+      </button>
+    </li>
+  )
+}
+export default TabItem
